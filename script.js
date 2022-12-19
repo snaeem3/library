1198d7abc6a420fc9e511d87fbd0fa212e83d4af
Sameer <sameer4363@gmail.com>
2022-12-18 22:06:18 -0500

    Add toggle switch and initial switch setting

--- script.js
+++ script.js
@@ -49,8 +49,16 @@ function displayLibrary() {
     // Loop through each book in library
     let index = 0;
     myLibrary.forEach((book) => {
-        // console.log(index);
+        console.log(index);
         const bookTable = document.createElement('table');
+        const readSwitch = document.createElement('input');
+        const readLabel = document.createElement('label');
+        readSwitch.setAttribute('type','checkbox');
+        readSwitch.setAttribute('class','checkbox');
+        readSwitch.setAttribute('id','switch' + index);
+        readLabel.setAttribute('for','switch' + index)
+        readLabel.setAttribute('class','toggle');
+
         for (const [key,value] of Object.entries(book)) {
             // console.log(`${key}: ${value}`);
             const tr = document.createElement('tr');
@@ -62,34 +70,44 @@ function displayLibrary() {
 
             th.appendChild(thText);
             td.appendChild(tdText);
+            td.setAttribute('class',key);
 
             tr.appendChild(th);
             tr.appendChild(td);
             bookTable.appendChild(tr);
         }
+        console.log(bookTable.rows[3].cells[1].innerText);
+        if (bookTable.rows[3].cells[1].innerText === 'true') {
+            readSwitch.setAttribute('checked','checked');
+        }
         contentDiv.appendChild(bookTable);
+        // const readLabelText = document.createElement('p');
 
-        const readSwitch = document.createElement('input');
-        const readLabel = document.createElement('label');
-        const readLabelText = document.createElement('p');
+        readSwitch.addEventListener('change', (event) => {
+            if (event.currentTarget.checked) {
+                console.log('checked');
+            } else {
+                console.log('unchecked');
+            }
+            updateReadValue(book);
+        })
+        // readLabelText.innerText = 'UNREAD    READ';
 
-        readSwitch.setAttribute('type','checkbox');
-        readSwitch.setAttribute('id','switch' + index);
-        readLabel.setAttribute('for','switch' + index)
-        readLabel.setAttribute('class','toggle');
-        readLabelText.innerText = 'UNREAD    READ';
-
-        readLabel.appendChild(readLabelText);
+        // readLabel.appendChild(readLabelText);
         contentDiv.appendChild(readSwitch);
         contentDiv.appendChild(readLabel);
         ++index;
     })
 }
 
+function updateReadValue(book) {
+    
+}
+
 displayLibrary();
 console.log(myLibrary);
 let book1 = new Book("best book","me",214,true);
-let book2 = new Book("worst book","you",214,true);
+let book2 = new Book("worst book","you",214,false);
 addBookToLibrary(book1);
 addBookToLibrary(book2);
 console.log(myLibrary);
